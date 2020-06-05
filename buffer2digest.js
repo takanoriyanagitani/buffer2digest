8b68501ed0e4c3861ef8f3409472f4cc3e77e2b4
yanagitani <takanori.yanagitani@gmail.com>
2020-06-05 10:35:48 +0900

    trying to test using chrome&selenium...

---
+++ buffer2digest.js
@@ -0,0 +1,50 @@
+(function(g,f){
+
+  const n = [ typeof module ]
+    .filter(function(t){ return "object" === t })
+    .map(function(_){ return typeof module.exports })
+    .filter(function(t){ return "object" === t })
+    .map(function(_){ return "node" })
+
+  const ie11 = [ typeof msCrypto ]
+    .filter(function(t){ return "object" === t })
+    .map(function(_){ return "ie11" })
+
+  const b = [ typeof crypto ]
+    .filter(function(t){ return "object" === t })
+    .map(function(_){ return "browser" })
+
+  const type = n[0] || ie11[0] || b[0]
+
+  switch(type){
+    default: break
+    case "node": return f(module.exports, "node")
+    case "browser":
+    case "ie11":
+      g.Buffer2Digest = {}
+      return f(g.Buffer2Digest, type)
+  }
+
+})(this, function(e, type){
+switch(type){
+  default: break
+  case "browser":
+    e.buffer2digest = function(ab, algorithm, callback){
+      return window.crypto.subtle.digest(algorithm, ab)
+      .then(callback)
+    }
+    break
+  case "node":
+    const crypto = require("crypto")
+    e.buffer2digest_buf = function(ab, algorithm){
+      const h = crypto.createHash(algorithm)
+      h.update(Buffer.from(ab))
+      return h.digest()
+    }
+    e.buffer2digest = function(ab, algorithm, callback){
+      const b = e.buffer2digest_buf(ab, algorithm)
+      callback(b.buffer)
+    }
+    break
+}
+})
---
+++ buffer2digest_test.js
@@ -0,0 +1,97 @@
+const path = require("path")
+const fs   = require("fs")
+const http = require("http")
+
+const handle_file = (q,s, name, type="text/html; charset=UTF-8") => {
+  s.setHeader("Content-Type", type)
+  return new Promise(r=>fs.readFile(name, {encoding:null, flag:"r"}, (err,data)=>r({err,data})))
+  .then(r=>{
+    const {
+      err,
+      data,
+    } = r
+    const ng = [ err ]
+      .filter(e => !! e)
+      .map(_ => {
+	s.setHeader("Content-Type", "text/plain; charset=UTF-8")
+	s.statusCode = 500
+	s.end("error.")
+      })
+    const ok = [ err ]
+      .filter(e => ! e)
+      .map(_ => {
+	s.statusCode = 200
+	s.end(data)
+      })
+  })
+}
+
+const handler = (q,s)=>{
+  const { url } = q
+  switch(url){
+    default:      return s.end("")
+    case "/zero": return handle_file(q,s, "./test/zero/index.html")
+    case "/buffer2digest.js": return handle_file(q,s, "./buffer2digest.js", "application/javascript")
+  }
+}
+
+const { Builder } = require("selenium-webdriver")
+
+const chrome = require("selenium-webdriver/chrome")
+
+const timeout = 65536
+const port = 60580
+
+require("chromedriver")
+
+const Buffer2Digest = require("./buffer2digest")
+
+describe("node", () => {
+
+  describe("buffer2digest", () => {
+
+    test("zero", () => Promise.resolve(new ArrayBuffer(32))
+      .then(ab => new Promise(r=>Buffer2Digest.buffer2digest(ab, "SHA256", b=>r(b))))
+      .then(ab => Buffer.from(ab))
+      .then(buf => buf.toString("hex"))
+      .then(s => expect(s).toBe("66687aadf862bd776c8fc18b8e9f8e20089714856ee233b3902a591d0d5f2925"))
+    )
+
+  })
+
+})
+
+describe("chrome", () => {
+
+  describe("buffer2digest", () => {
+
+    const state = {
+      browser: null,
+      server:  null,
+    }
+
+    beforeAll(() => Promise.resolve(new Builder())
+      .then(b=>b.forBrowser("chrome"))
+      .then(b=>b.setChromeOptions(new chrome.Options().headless()))
+      .then(b=>b.build())
+      .then(b=>Object.assign(state, {browser:b}))
+      .then(_=>http.createServer(handler))
+      .then(s=>Object.assign(state, {server:s}))
+      .then(_=>state.server.listen(port))
+    , timeout)
+
+    afterAll(() => Promise.resolve(state.browser)
+      .then(b=>b.quit())
+      .then(_=>state.server)
+      .then(s=>new Promise(r=>s.close(r)))
+    , timeout)
+
+    test("zero", () => Promise.resolve(state.browser)
+      .then(b=>b.get("http://localhost:"+port+"/zero"))
+      .then(_=>state.browser.getTitle())
+      //.then(t=>expect(t).toBe("66687aadf862bd776c8fc18b8e9f8e20089714856ee233b3902a591d0d5f2925"))
+    )
+
+  })
+
+})
